c07d8120adcc11c67a0edcc97519305d700a6ffb
Alina-kio <95016284+Alina-kio@users.noreply.github.com>
2022-05-13 17:37:31 +0600

    Update client_kb.py

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,8 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+start_button = KeyboardButton("/start")
 mem_button = KeyboardButton("/mem")
 quiz_button = KeyboardButton("/quiz")
 
 start_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-start_markup.row(mem_button, quiz_button)
+start_markup.row(start_button, mem_button, quiz_button)
