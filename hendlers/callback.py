79368e6f81f53be95a3f68175225de17c6e5c0dc
Alina-kio <95016284+Alina-kio@users.noreply.github.com>
2022-05-13 17:36:16 +0600

    Update callback.py

--- hendlers/callback.py
+++ hendlers/callback.py
@@ -3,29 +3,6 @@ from config import bot, dp
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 
 
-# async def quiz_3(call: types.CallbackQuery):
-#     question = "Ответы:"
-#     answers = [
-#         '4',
-#         '8',
-#         '4, 6',
-#         '2, 4'
-#         '5',
-#     ]
-#     photo = open("media/problem1.jpg", "rb")
-#     await bot.send_photo(call.from_user.id, photo=photo)
-#     await bot.send_poll(
-#         chat_id=call.from_user.id,
-#         question=question,
-#         options=answers,
-#         is_anonymous=False,
-#         type='quiz',
-#         correct_option_id=3,
-#         explanation="Это же легко",
-#         explanation_parse_mode=ParseMode.MARKDOWN_V2,
-#     )
-
-
 async def quiz_2(call: types.CallbackQuery):
     markup = InlineKeyboardMarkup()
     button_call_2 = InlineKeyboardButton("NEXT", callback_data="button_call_2")
